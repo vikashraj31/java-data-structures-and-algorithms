d93849b6ed7e2bd8ef5df1bc072b54e066ee4614
vikashvanshi <vikashh3121@gmail.com>
2026-04-30 18:52:48 +0530

    Diamond

---
+++ Pattern/diamond.java
@@ -0,0 +1,36 @@
+package Pattern;
+
+public class diamond {
+
+    public static void Diamond(int n){
+        // Top
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= (2*i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom
+        for(int i = n - 1; i >= 1; i--){
+
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= (2*i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Diamond(7);
+    }
+}
